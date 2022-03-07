1215a2403d87e099ae23d42c19947b523dec9ce7
cetherdev <robert.hct@gmail.com>
2022-03-07 11:25:18 -0600

    fix: Routes with version and api word

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,14 @@
+const express = require('express');
 const productsRouter = require('./products');
 const categoriesRouter = require('./categories');
 const usersRouter = require('./users');
 
 function routerApi(app) {
-  app.use('/products', productsRouter);
-  app.use('/categories', categoriesRouter);
-  app.use('/users', usersRouter);
+  const router = express.Router();
+  app.use('/api/v1', router);
+  router.use('/products', productsRouter);
+  router.use('/categories', categoriesRouter);
+  router.use('/users', usersRouter);
 }
 
 module.exports = routerApi;
