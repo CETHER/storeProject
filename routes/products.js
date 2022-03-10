c91a6af525caf611d0bb1112a70686fe373e3b8d
cetherdev <robert.hct@gmail.com>
2022-03-10 10:41:01 -0600

    feat: Create the services from product- (create, find, findOne, update, delete)

--- routes/products.js
+++ routes/products.js
@@ -1,22 +1,13 @@
-const faker = require('faker');
 const express = require('express');
+const ProductService = require('../services/product.js');
 
 const router = express.Router();
+const productService = new ProductService();
 
 router.get('/', (req, res) => {
-  const products = [];
-  const { size } = req.query;
-  const limit = size || 10;
-  for (let i = 0; i < limit; i++) {
-    products.push({
-      name: faker.commerce.productName(),
-      price: parseInt(faker.commerce.price(), 10),
-      price: faker.image.imageUrl(),
-    });
-  }
+  const products = productService.find();
   res.json(products);
 });
-
 
 router.get('/filter', (req, res) => {
   res.send('Yo soy un fileter');
@@ -24,37 +15,32 @@ router.get('/filter', (req, res) => {
 
 router.get('/:id', (req, res) => {
   const { id } = req.params;
-
-  if (id === '999') {
-    res.status(404).json({
-      message: 'Product not found',
-    });
-  } else {
-    res.status(200).json({
-      id,
-      name: 'Product 2',
-      price: 800,
-    });
-  }
+  const product = productService.findOne(id);
+  res.json(product);
 });
 
 router.post('/', (req, res) => {
   const body = req.body;
+  const newProduct = productService.create(body);
 
-  res.json({ message: 'created', data: body });
+  res.status(201).json({ message: 'created', data: newProduct });
 });
 
 router.patch('/:id', (req, res) => {
   const { id } = req.params;
   const body = req.body;
 
-  res.status(201).json({ message: 'updated', data: body, id });
+  const productUpdated = productService.update(id, body);
+
+  res.status(201).json({ message: 'updated', data: productUpdated });
 });
 
 router.delete('/:id', (req, res) => {
   const { id } = req.params;
 
-  res.json({ message: 'deleted', id });
+  const productDeleted = productService.delete(id);
+
+  res.json({ message: 'deleted', productDeleted });
 });
 
 module.exports = router;
